5c2c3bb03375442c8ba7121c6cef97eac9d5e834
navjots <navjots@geekyants.com>
2023-04-23 14:25:20 +0530

    updated config file

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
-import * as dotenv from 'dotenv'
+import { MONGODB_URI } from './config.js'
 
-dotenv.config()
-
-const uri = process.env.MONGODB_URI
+const uri = MONGODB_URI
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import express from 'express'
-import * as dotenv from 'dotenv'
-// eslint-disable-next-line import/extensions
+import { PORT } from './config.js'
 import connectToMongo from './db.js'
-
-dotenv.config()
 
 await connectToMongo()
 
 const app = express()
-const { PORT } = process.env
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
