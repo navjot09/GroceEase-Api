224952f966069b81a7638c0642f4c5ce39efb8b5
navjots <navjots@geekyants.com>
2023-11-06 20:10:17 +0530

    added admin auth and middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
-import { createUser, loginUser } from '@controllers/auth'
+import { createAdmin, createUser, loginAdmin, loginUser } from '@controllers/auth'
 import { Router } from 'express'
 
 const router: Router = Router()
 
 router.post('/createUser', createUser)
 router.post('/loginUser', loginUser)
+router.post('/createAdmin', createAdmin)
+router.post('/loginAdmin', loginAdmin)
 
 export default router
